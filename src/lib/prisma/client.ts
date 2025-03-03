991155e05b54210fb3906a8298ad7678ba0c025f
菅野 <133845293+kit4000@users.noreply.github.com>
2025-03-03 14:10:27 +0900

    Add Prisma client singleton

--- src/lib/prisma/client.ts
+++ src/lib/prisma/client.ts
@@ -1,9 +1,18 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
+// PrismaClientのグローバル変数の型を拡張
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma = global.prisma || new PrismaClient();
+// 開発環境での不要な複数のPrismaClientインスタンス生成を防ぐ
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  });
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+// 開発環境でのみグローバル変数にPrismaClientを割り当て
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
